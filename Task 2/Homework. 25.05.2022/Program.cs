3c3ad7e8ce3da0c766b66344a5d7fb48df3b772f
Khagani89 <khaganichh@code.edu.az>
2022-05-26 03:23:29 +0400

    project

---
+++ Task 2/Homework. 25.05.2022/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework._25._05._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verilmis adlar siyahisinda (string array) nece adin uzunlugunun 5-den boyuk oldugunu tapan proqram
+
+            string[] namelist = { "Ali","Xaliq","Xaqani","Mehemmed" };
+            int count = 0;
+            {
+                for (int i = 0; i < namelist.Length; i++) 
+                {
+                    for(int j = 0; j < namelist[i].Length; j++)
+                    {
+                        if(j > 5)
+                        {
+                            count++;
+                        }
+                    }
+                }  
+
+            }Console.WriteLine(count);
+        }
+    }
+}
---
+++ Task 3/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+        
+        var cutededler = List(1,2,3,4,5,6,7,8,9);
+
+         Console.WriteLine(cutededler);
+            
+        
+        }
+
+        static int List(int[] numbers)
+        {
+          
+            int numbers2 = 0;
+            {
+
+                for (int i = 0; i < numbers.Length; i++) 
+                {
+                    if (numbers[i] % 2 == 0) ;
+                    {
+                   numbers2 = numbers[i];
+
+                    }
+                }
+                return numbers2;    
+                
+                     
+            }
+          
+
+
+
+        
+        }
+    }
+}
